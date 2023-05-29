3133ab8a0716de9db3cbb055873e77dfe33e3e3c
MyungJiwoo <1206jiwoo@gmail.com>
2023-05-29 22:07:47 +0900

    P12925

---
+++ src/programmers/P12925.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class P12925 {
+    public static void main(String[] args) {
+        String s = "1234";
+        int answer = 0;
+        answer = Integer.valueOf(s);
+    }
+}
+
+// P12925 : 문자열을 정수로 바꾸기
