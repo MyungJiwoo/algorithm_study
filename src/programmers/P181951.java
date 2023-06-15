22b3839d532503972b4d1f7ac572a8bc1deb85c8
MyungJiwoo <1206jiwoo@gmail.com>
2023-06-15 23:42:49 +0900

    P181951

---
+++ src/programmers/P181951.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class P181951 {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+
+	        System.out.println("a = " +  a);
+	        System.out.println("b = " +  b);
+	        
+	        sc.close();
+	    }
+}
+
+// P181951: a와 b 출력하기
