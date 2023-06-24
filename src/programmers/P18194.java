3f8ba60fdd05996813c2dc5149865d67e6809475
MyungJiwoo <1206jiwoo@gmail.com>
2023-06-24 23:46:58 +0900

    P18194

---
+++ src/programmers/P18194.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class P18194 {
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+            String b = sc.next();
+            System.out.println(a + b);
+        }
+    }
+}
