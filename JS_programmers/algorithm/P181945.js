22d0e0d51faf5211b82c0b03d1fa1f5c347664eb
MyungJiwoo <1206jiwoo@gmail.com>
2023-07-01 00:54:07 +0900

    P12910

---
+++ JS_programmers/algorithm/P181945.js
@@ -0,0 +1,13 @@
+import { createInterface } from "readline";
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0];
+});
---
+++ JS_programmers/algorithm/test.js
@@ -0,0 +1,5 @@
+let answer = 0;
+let num1 = 10;
+let num2 = 5;
+answer = Math.floor(num1 / num2);
+console.log(answer);
