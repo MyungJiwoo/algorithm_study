83769f5d5cba3bee95d55ec114e1e07b0c3d3b45
MyungJiwoo <1206jiwoo@gmail.com>
2024-12-07 02:37:17 +0900

    P120910

---
+++ JS_algorithm/P120910.js
@@ -0,0 +1,12 @@
+function solution(n, t) {
+    let answer = n;
+
+    for (let i = 0; i < t; i++) {
+        answer *= 2;
+    }
+
+    return answer;
+}
+
+console.log(solution(2, 10));
+console.log(solution(7, 15));
